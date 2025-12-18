1fe11cee3785660f72bb03b1312650e0d6a09d88
matthieu <matthieu.comont-burguet@cgi.com>
2025-12-18 16:01:53 +0100

    Ajout Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
 
-      <h1></h1>
+      <h1>
+        <p className="underline underline-offset-1">The quick brown fox...</p>
+      </h1>
     </>
   )
 }
